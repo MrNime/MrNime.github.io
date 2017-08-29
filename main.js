c4430f468513f28b033f9909a8317ba2d8fe00ae
Nicky Meuleman <nicky.meuleman@gmail.com>
2017-08-29 23:34:41 +0200

    extra FAB, scrollspy fix and extra project

--- main.js
+++ main.js
@@ -1,4 +1,13 @@
 $(document).ready(function(){
     $('.scrollspy').scrollSpy();
     $('.parallax').parallax();
+    $(".button-collapse").sideNav();
+
+    //scrollspy active on li tags that are parents of the a tags that get affected
+    $('.scrollspy').on('scrollSpy:enter', function() {
+    $('.navbar-fixed').find('a[href="#'+$(this).attr('id')+'"]').parent().addClass('active'); //add active class to the li of the nav link
+    });
+    $('.scrollspy').on('scrollSpy:exit', function() {
+    $('.navbar-fixed').find('a[href="#'+$(this).attr('id')+'"]').parent().removeClass('active'); //remove active class to the li of the nav link
+    });
   });
